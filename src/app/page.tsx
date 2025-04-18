d421ad02bc643f39fced84b98899a93667ea61fc
mui-1729 <s1f102503015@iniad.org>
2025-04-18 21:56:01 +0900

    成長したい

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.board}>a</div>
+      <div className={styles.board}>i</div>
     </div>
   );
 }
