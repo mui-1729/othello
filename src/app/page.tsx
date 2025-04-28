7b51de333d5f3fea6a51f99a2a4b28ec2a9771b1
mui-1729 <s1f102503015@iniad.org>
2025-04-28 17:14:24 +0900

    '下'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,68 +12,93 @@ export default function Home() {
     [0, 0, 0, 0, 0, 2, 1, 2],
     [0, 0, 0, 0, 1, 2, 1, 2],
     [0, 0, 0, 2, 1, 2, 1, 2],
-    [0, 0, 1, 2, 1, 2, 1, 2],
-    [0, 1, 2, 1, 2, 1, 2, 1],
+    [2, 0, 1, 2, 1, 2, 1, 2],
+    [1, 2, 2, 1, 2, 1, 2, 1],
   ]);
   const clickHandler = (x: number, y: number) => {
-    console.log(x, y);
     const newBoard = structuredClone(board);
-    {
-      newBoard[y][x] = turnColor;
-      setTurnColor(3 - turnColor);
-    }
+    // if (board[y + 1] === undefined) return;
 
-    if (
-      board[y + 1] !== undefined &&
-      board[y + 1][x] === 3 - turnColor &&
-      board[y + 2][x] === turnColor
-    )
-      setBoard(newBoard);
-    else if (
-      board[y + 1] !== undefined &&
-      board[y + 1][x] &&
-      board[y + 2][x] === 3 - turnColor &&
-      board[y + 3][x] === turnColor
-    )
-      setBoard(newBoard);
-    else if (
-      board[y + 1] !== undefined &&
-      board[y + 1][x] &&
-      board[y + 2][x] &&
-      board[y + 3][x] === 3 - turnColor &&
-      board[y + 4][x] === turnColor
-    )
-      setBoard(newBoard);
-    else if (
-      board[y + 1] !== undefined &&
-      board[y + 1][x] &&
-      board[y + 2][x] &&
-      board[y + 3][x] &&
-      board[y + 4][x] === 3 - turnColor &&
-      board[y + 5][x] === turnColor
-    )
-      setBoard(newBoard);
-    else if (
-      board[y + 1] !== undefined &&
-      board[y + 1][x] &&
-      board[y + 2][x] &&
-      board[y + 3][x] &&
-      board[y + 4][x] &&
-      board[y + 5][x] === 3 - turnColor &&
-      board[y + 6][x] === turnColor
-    )
-      setBoard(newBoard);
-    else if (
-      board[y + 1] !== undefined &&
-      board[y + 1][x] &&
-      board[y + 2][x] &&
-      board[y + 3][x] &&
-      board[y + 4][x] &&
-      board[y + 5][x] &&
-      board[y + 6][x] === 3 - turnColor &&
-      board[y + 7][x] === turnColor
-    )
-      setBoard(newBoard);
+    console.log(x, y);
+
+    if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor)
+      if (board[y + 2] === undefined) {
+        return;
+      } else if (board[y + 2][x] === turnColor) {
+        newBoard[y + 1][x] = turnColor;
+        newBoard[y][x] = turnColor;
+        setBoard(newBoard);
+        setTurnColor(3 - turnColor);
+        return;
+      } else if (board[y + 2][x] === 3 - turnColor && board[y + 3][x] === turnColor) {
+        newBoard[y + 1][x] = turnColor;
+        newBoard[y + 2][x] = turnColor;
+        newBoard[y][x] = turnColor;
+        setBoard(newBoard);
+        setTurnColor(3 - turnColor);
+        return;
+      } else if (
+        board[y + 2][x] &&
+        board[y + 3][x] === 3 - turnColor &&
+        board[y + 4][x] === turnColor
+      ) {
+        newBoard[y + 1][x] = turnColor;
+        newBoard[y + 2][x] = turnColor;
+        newBoard[y + 3][x] = turnColor;
+        newBoard[y][x] = turnColor;
+        setBoard(newBoard);
+        setTurnColor(3 - turnColor);
+        return;
+      } else if (
+        board[y + 2][x] &&
+        board[y + 3][x] &&
+        board[y + 4][x] === 3 - turnColor &&
+        board[y + 5][x] === turnColor
+      ) {
+        newBoard[y + 1][x] = turnColor;
+        newBoard[y + 2][x] = turnColor;
+        newBoard[y + 3][x] = turnColor;
+        newBoard[y + 4][x] = turnColor;
+        newBoard[y][x] = turnColor;
+        setBoard(newBoard);
+        setTurnColor(3 - turnColor);
+        return;
+      } else if (
+        board[y + 2][x] &&
+        board[y + 3][x] &&
+        board[y + 4][x] &&
+        board[y + 5][x] === 3 - turnColor &&
+        board[y + 6][x] === turnColor
+      ) {
+        newBoard[y + 1][x] = turnColor;
+        newBoard[y + 2][x] = turnColor;
+        newBoard[y + 3][x] = turnColor;
+        newBoard[y + 4][x] = turnColor;
+        newBoard[y + 5][x] = turnColor;
+        newBoard[y][x] = turnColor;
+        setBoard(newBoard);
+        setTurnColor(3 - turnColor);
+        return;
+      } else if (
+        board[y + 2][x] &&
+        board[y + 3][x] &&
+        board[y + 4][x] &&
+        board[y + 5][x] &&
+        board[y + 6][x] === 3 - turnColor &&
+        board[y + 7][x] === turnColor
+      ) {
+        newBoard[y + 1][x] = turnColor;
+        newBoard[y + 2][x] = turnColor;
+        newBoard[y + 3][x] = turnColor;
+        newBoard[y + 4][x] = turnColor;
+        newBoard[y + 5][x] = turnColor;
+        newBoard[y + 6][x] = turnColor;
+        newBoard[y][x] = turnColor;
+        setBoard(newBoard);
+        setTurnColor(3 - turnColor);
+        return;
+      }
+    
   };
   return (
     <div className={styles.container}>
